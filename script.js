0f48412005048bb74884412bcb6015c3e1f76334
vietduc0712 <159755410+vietduc0712@users.noreply.github.com>
2024-02-13 20:35:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function showMessage(response) {
 
     // Update the text content, display the message, and change the image to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "Yeahhhhhhhhhhh! I find me handsome too !!";
+    yesMessage.textContent = "Yeahhhh! I find me handsome too !!";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
